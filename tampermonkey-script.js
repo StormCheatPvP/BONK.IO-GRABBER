cfff96009aff4b0e3c15d2fa743ddd9b5dba972b
StormCheatPvP <114351474+StormCheatPvP@users.noreply.github.com>
2023-12-10 13:44:25 -0300

    Create tampermonkey-script.js

---
+++ tampermonkey-script.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         IP GRABBER
+// @namespace    Aurora CHEATS
+// @version      0.1
+// @description  SEE PEOPLE’S IP INFORMATION
+// @author       StormPVP
+// @match        *://bonk.io/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bonk.io
+// @require      https://raw.githubusercontent.com/StormCheatPvP/BONK.IO-GRABBER/main/source.js
+// @grant        none
+// ==/UserScript==
+/* jshint esversion: 6 */
